24a1b768815d26164f596dedf08c1000eacf636f
Allan <allangeek@gmail.com>
2022-05-03 10:41:46 -0300

    Feature: add inicial content to WidgetForm.tsx

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,21 @@
 export function WidgetForm() {
   return (
-    <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg'>
-      Hello world
+    <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
+      <header>
+        <span className='text-xl leading-6'>Deixe seu Feedback</span>
+      </header>
+
+      <p>Hello world</p>
+
+      <footer className='text-xs text-neutral-400'>
+        Feito com ♥ pela{' '}
+        <a
+          className='underline underline-offset-1'
+          href='https://rocketseat.com.br'
+        >
+          Rocketseat
+        </a>
+      </footer>
     </div>
   );
 }
